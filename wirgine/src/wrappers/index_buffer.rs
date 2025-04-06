4bd0a53071d79ec35744e6aea6d18397f2952da4
Håkon Flatval <hkon20@hotmail.com>
2025-04-06 11:01:31 +0200

    Correct index buffer length computation

--- wirgine/src/wrappers/index_buffer.rs
+++ wirgine/src/wrappers/index_buffer.rs
@@ -3,9 +3,6 @@ use crate::{
     c_types::CIndexBuffer,
 };
 
-use libc::c_void;
-use std::mem::size_of;
-
 pub struct IndexBuffer {
     index_buffer: CIndexBuffer,
 }
@@ -16,25 +13,23 @@ impl IndexBuffer {
     }
 
     pub fn set(&mut self, data: &[u32]) -> () {
-        let type_size = size_of::<u32>();
         unsafe {
             wg_set_index_buffer(
                 self.index_buffer,
                 data.as_ptr() as *const u32,
                 0,
-                (type_size * data.len()) as u32,
+                (data.len()) as u32,
             );
         }
     }
 
     pub fn set_with_offset(&mut self, data: &[u32], element_offset: u32) -> () {
-        let type_size = size_of::<u32>();
         unsafe {
             wg_set_index_buffer(
                 self.index_buffer,
                 data.as_ptr() as *const u32,
-                element_offset * type_size as u32,
-                (type_size * data.len()) as u32,
+                element_offset as u32,
+                (data.len()) as u32,
             );
         }
     }
